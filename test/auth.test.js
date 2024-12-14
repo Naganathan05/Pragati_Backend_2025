4efbcc9d39e0dea5f15826c80f12dcc2dea60211
Thanus Kumaar <thanuskumaara@gmail.com>
2024-12-14 14:25:25 +0530

    [ADD]: Created basic auth API test file for signup testing

---
+++ test/auth.test.js
@@ -0,0 +1,25 @@
+import * as chaiModule from "chai";
+import chaiHttp from "chai-http";
+import { describe, it } from "mocha";
+import { expect } from "chai";
+
+const server = "http://localhost:5000";
+const chai = chaiModule.use(chaiHttp);
+
+import { signupTestCases } from "./testCases/signup.js";
+
+describe("POST - /api/auth/signup", () => {
+  signupTestCases.forEach(
+    ({ name, payload, expectedStatus, expectedMessage, expectedData }) => {
+      it(name, async () => {
+        const res = await chai
+          .request.execute(server)
+          .post("/api/auth/signup")
+          .send(payload);
+        expect(res).to.have.status(expectedStatus);
+        expect(res.body).to.have.property("MESSAGE", expectedMessage);
+        expect(res.body).to.have.haveOwnProperty("DATA");
+      });
+    }
+  );
+});
