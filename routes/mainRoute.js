e6fbfb8d3b8ae8f61938244f2435f0fe7bfffc9d
Naganathan05 <umaiyalramesh@gmail.com>
2024-12-25 10:39:08 +0530

    Conflicts resolved and Unwanted Checks removed.

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import authRouter from "./authRoute.js";
 import eventRouter from "./eventRoute.js";
+import tagRouter from "./tagRoute.js";
 import organizerRouter from "./organizerRoute.js";
+import adminRouter from "./adminRoute.js";
 import registrationRouter from "./registrationRoute.js";
 
 const router = Router();
 
-router.use("/auth", authRouter);
 router.use("/event", eventRouter);
-router.use('/org',organizerRouter)
+router.use('/org',organizerRouter);
+router.use('/admin', adminRouter);
+router.use('/auth',authRouter);
+router.use('/tag', tagRouter); 
 router.use('/registration', registrationRouter);
 
 export default router;
