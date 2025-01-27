7f736a7ec7e8abe1950e90ce61ac99f98c028a2a
Thanus Kumaar <thanuskumaara@gmail.com>
2025-01-27 19:58:06 +0530

    [ADD]: Added docs for signup and forgot password.

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -28,9 +28,87 @@ const authRouter = Router();
  *         description: Login successful
  *       400:
  *         description: Invalid credentials
+ *       500:
+ *         description: A problem from our side :(
  */
 authRouter.post("/login", authController.login);
+
+/**
+ * @swagger
+ * /api/auth/forgotPassword:
+ *   post:
+ *     summary: Route for getting OTP for forgot password
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userEmail:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       400:
+ *         description: Invalid Email address
+ *       500:
+ *         description: A problem from our side :(
+ */
 authRouter.post("/forgotPassword", authController.forgotPassword);
+
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     summary: Route for user sign up
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userEmail:
+ *                 type: string
+ *               userPassword:
+ *                 type: string
+ *               userName:
+ *                 type: string
+ *               rollNumber:
+ *                 type: string
+ *               phoneNumber:
+ *                 type: string
+ *               collegeName:
+ *                 type: string
+ *               collegeCity:
+ *                 type: string
+ *               userDepartment:
+ *                 type: string
+ *               academicYear:
+ *                 type: number
+ *               degree:
+ *                 type: string
+ *               needAccommodationDay1:
+ *                 type: boolean
+ *               needAccommodationDay2:
+ *                 type: boolean
+ *               needAccommodationDay3:
+ *                 type: boolean
+ *               isAmrita:
+ *                 type: boolean
+ *     responses:
+ *       201:
+ *         description: User registration successful
+ *       400:
+ *         description: Invalid input provided
+ *       500:
+ *         description: A problem from our side :(
+ */
 
 authRouter.post("/signup", authController.signup);
 
