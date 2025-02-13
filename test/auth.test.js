0f27eee76fc91b16e0c281202222bae6d3795d02
Thanus Kumaar <thanuskumaara@gmail.com>
2025-02-13 20:03:41 +0530

    [FORMAT]

--- test/auth.test.js
+++ test/auth.test.js
@@ -9,17 +9,17 @@ const chai = chaiModule.use(chaiHttp);
 import { signupTestCases } from "./testCases/signup.js";
 
 describe("POST - /api/auth/signup", () => {
-  signupTestCases.forEach(
-    ({ name, payload, expectedStatus, expectedMessage, expectedData }) => {
-      it(name, async () => {
-        const res = await chai
-          .request.execute(server)
-          .post("/api/auth/signup")
-          .send(payload);
-        expect(res).to.have.status(expectedStatus);
-        expect(res.body).to.have.property("MESSAGE", expectedMessage);
-        expect(res.body).to.have.haveOwnProperty("DATA");
-      });
-    }
-  );
+    signupTestCases.forEach(
+        ({ name, payload, expectedStatus, expectedMessage, expectedData }) => {
+            it(name, async () => {
+                const res = await chai.request
+                    .execute(server)
+                    .post("/api/auth/signup")
+                    .send(payload);
+                expect(res).to.have.status(expectedStatus);
+                expect(res.body).to.have.property("MESSAGE", expectedMessage);
+                expect(res.body).to.have.haveOwnProperty("DATA");
+            });
+        },
+    );
 });
